3674387ad96c912039d7217b69b1dea75df06778
zengzhiwen <zengzhiwen@ky-tech.com.cn>
2021-02-04 21:36:41 +0800

    0606_01

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ import sys
 import pygame
 from my_plane import MyPlane
 
+# 在水平方向上，窗口尺寸占电脑屏幕尺寸的比例
+SCALE_HORIZONTAL = 2 / 5
+# 在竖直方向上，窗口尺寸占电脑屏幕尺寸的比例
+SCALE_VERTICAL = 4 / 5
+
+# 动画的最大帧率
+MAX_FRAMERATE = 10
+
 class PlaneWar:
     """管理游戏的总体类"""
 
@@ -13,10 +21,40 @@ class PlaneWar:
         # 初始化pygame库
         pygame.init()
 
-        screen_width = 700
-        screen_height = 900
+        # 获得当前电脑屏幕的尺寸
+        screen_width, screen_height = self.get_screen_siz()
+        # 根据当前电脑屏幕的尺寸计算窗口的尺寸
+        window_width, window_height = screen_width * SCALE_HORIZONTAL, screen_height * SCALE_VERTICAL
         # 创建指定尺寸的窗口（游戏画面的所有元素都将在创建的这个窗口中绘制）
-        self.window = pygame.display.set_mode([screen_width, screen_height])
+        self.window = pygame.display.set_mode((round(window_width), round(window_height)))
+
+        # 创建全屏模式的窗口（游戏画面的所有元素都将在创建的这个窗口中绘制）
+        # pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        
+        # 设置窗口
+        self._set_window()
+
+        # 创建一架我方飞机
+        self.my_plane = MyPlane(self.window)
+
+        # 创建一个用于跟踪时间的时钟对象
+        self.clock = pygame.time.Clock()
+
+    def get_screen_siz(self):
+        """获取当前电脑屏幕的尺寸"""
+
+        # 创建一个视频显示信息对象
+        info = pygame.display.Info()
+        # 获得当前电脑屏幕的宽度
+        screen_width = info.current_w
+        # 获得当前电脑屏幕的高度
+        screen_height = info.current_h
+
+        # 返回当前电脑屏幕的尺寸
+        return screen_width, screen_height
+
+    def _set_window(self):
+        """设置窗口"""
 
         # 设置窗口的标题
         pygame.display.set_caption("飞机大战")
@@ -27,25 +65,13 @@ class PlaneWar:
         # 设置窗口的图标
         pygame.display.set_icon(window_icon)
 
-        # 创建一架我方飞机
-        self.my_plane = MyPlane(self.window, (299, 824))
-
-        # 创建一个用于跟踪时间的时钟对象
-        self.clock = pygame.time.Clock()
-
     def run_game(self):
         """运行游戏"""
 
         # 让创建的窗口一直显示
         while True:
-            # 从事件队列中将所有事件全部取出并逐个进行处理
-            for event in pygame.event.get():
-                # 如果某个事件是退出程序
-                if event.type == pygame.QUIT:
-                    # 卸载pygame库
-                    pygame.quit()
-                    # 退出程序
-                    sys.exit()
+            # 处理事件
+            self._handle_events()
 
             # 设置窗口的背景色
             self.window.fill(pygame.Color('gray'))
@@ -60,7 +86,68 @@ class PlaneWar:
             self.my_plane.update()
 
             # 设置while循环体在一秒内执行的最大次数（设置动画的最大帧率）
-            self.clock.tick(10)
+            self.clock.tick(MAX_FRAMERATE)
+
+    def _handle_events(self):
+        """处理事件"""
+
+        # 从事件队列中将所有事件全部取出并逐个进行处理
+        for event in pygame.event.get():
+            # 如果某个事件是退出程序
+            if event.type == pygame.QUIT:
+                # 卸载 pygame 库
+                pygame.quit()
+                # 退出程序
+                sys.exit()
+            # 如果某个事件是用户按下了键盘上的某个键
+            elif event.type == pygame.KEYDOWN:
+                # 处理键盘按下的事件
+                self._handle_keydown_events(event)
+            # 如果某个事件是用户松开了键盘上的某个键
+            elif event.type == pygame.KEYUP:
+                # 处理键盘松开的事件
+                self._handle_keyup_events(event)
+
+    def _handle_keydown_events(self, event):
+        """处理键盘按下的事件"""
+
+        # 如果按下的键是上箭头
+        if event.key == pygame.K_UP:
+            # 标记我方飞机向上移动
+            self.my_plane.is_move_up = True
+        # 如果按下的键是下箭头
+        elif event.key == pygame.K_DOWN:
+            # 标记我方飞机向下移动
+            self.my_plane.is_move_down = True
+        # 如果按下的键是左箭头
+        elif event.key == pygame.K_LEFT:
+            # 标记我方飞机向左移动
+            self.my_plane.is_move_left = True
+        # 如果按下的键是右箭头
+        elif event.key == pygame.K_RIGHT:
+            # 标记我方飞机向右移动
+            self.my_plane.is_move_right = True
+
+    def _handle_keyup_events(self, event):
+        """处理键盘松开的事件"""
+
+        # 如果松开的键是上箭头
+        if event.key == pygame.K_UP:
+            # 标记我方飞机不向上移动
+            self.my_plane.is_move_up = False
+        # 如果松开的键是下箭头
+        elif event.key == pygame.K_DOWN:
+            # 标记我方飞机不向下移动
+            self.my_plane.is_move_down = False
+        # 如果松开的键是左箭头
+        elif event.key == pygame.K_LEFT:
+            # 标记我方飞机不向左移动
+            self.my_plane.is_move_left = False
+        # 如果松开的键是右箭头
+        elif event.key == pygame.K_RIGHT:
+            # 标记我方飞机不向右移动
+            self.my_plane.is_move_right = False
+
 
 # 只有当直接运行main.py时
 if __name__ == '__main__':
--- my_plane.py
+++ my_plane.py
@@ -4,26 +4,55 @@ import pygame
 class MyPlane:
     """我方飞机类"""
 
-    def __init__(self, window, pos):
+    def __init__(self, window):
         """初始化我方飞机"""
 
         # 获得窗口对象
         self.window = window
 
-        # 获得我方飞机的位置
-        self.pos_x, self.pos_y = pos
-
         # 加载我方飞机图片
         self.image = pygame.image.load('images/my_plane.png')
 
+        # 获得我方飞机的的矩形
+        self.rect = self.image.get_rect()
+
+        self.window_rect = self.window.get_rect()
+
+        self.rect.midbottom = self.window_rect.midbottom
+
+        # 标记我方飞机不向上移动
+        self.is_move_up = False
+        # 标记我方飞机不向下移动
+        self.is_move_down = False
+        # 标记我方飞机不向左移动
+        self.is_move_left = False
+        # 标记我方飞机不向右移动
+        self.is_move_right = False
+
+        # 我方飞机每次移动时的偏移量
+        self.offset = 20
     def update(self):
         """更新我方飞机的位置"""
 
-        # 减少我方飞机的y坐标以向上移动
-        self.pos_y -= 20
+        # 如果我方飞机被标记为向上移动，并且向上移动后不会超过窗口的上边缘
+        if self.is_move_up and self.rect.top - self.offset > 0:
+            # 减少我方飞机的矩形的属性top以向上移动
+            self.rect.top -= self.offset
+        # 如果我方飞机被标记为向下移动，并且向下移动后不会超出窗口的下边缘
+        if self.is_move_down and self.rect.bottom + self.offset < self.window_rect.height:
+            # 增大我方飞机的矩形的属性bottom以向下移动
+            self.rect.bottom += self.offset
+        # 如果我方飞机被标记为向左移动，并且向左移动后不会超过窗口的左边缘
+        if self.is_move_left and self.rect.left - self.offset > 0:
+            # 减少我方飞机的矩形的属性left以向左移动
+            self.rect.left -= self.offset
+        # 如果我方飞机被标记为向右移动，并且向右移动不会超出窗口的右边缘
+        if self.is_move_right and self.rect.right + self.offset < self.window_rect.width:
+            # 增大我方飞机的矩形的属性right以向右移动
+            self.rect.right += self.offset
 
     def draw(self):
         """在窗口中绘制我方飞机"""
 
         # 在窗口的指定位置绘制一架我方飞机
-        self.window.blit(self.image, (self.pos_x, self.pos_y))
+        self.window.blit(self.image, self.rect)
